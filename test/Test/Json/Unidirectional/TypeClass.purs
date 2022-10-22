7aa7ee858dabc9870a506b839b8406e8da1de788
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-10-22 07:23:42 -0500

    Refactor test output

---
+++ test/Test/Json/Unidirectional/TypeClass.purs
@@ -0,0 +1,92 @@
+module Test.Json.Unidirectional.TypeClass where
+
+import Prelude
+
+import Data.Argonaut.Core (Json)
+import Data.Array as Array
+import Data.Either (either)
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Dodo (Doc)
+import Dodo.Ansi (GraphicsParam)
+import Effect (Effect)
+import Effect.Class.Console (log)
+import Foreign.Object (Object)
+import Foreign.Object as Object
+import Json.Decode.Class (decodeJson)
+import Json.Encode.Class (encodeJson)
+import Json.Errors.AnsiDodoError (printAnsiDodoError, runJsonDecoderADE)
+import Json.Primitive.Decode (JsonDecoder)
+import Json.Types (Optional(..))
+import Json.Unidirectional.Encode.Value (encodeBoolean, encodeArray, encodeInt, encodeNumber, encodeObject, encodeRecord, encodeString, encodeUnitToNull)
+
+runOutput :: Effect Unit
+runOutput = do
+  log "\n### Typeclass Output:"
+  runDecoderADE "Decode Record incorrectly" exampleRec
+    (decodeJson :: JsonDecoder (Doc GraphicsParam) Unit IncorrectRecordType)
+  runDecoderADE "Decode Record incorrectly (test optional field)" exampleRec
+    (decodeJson :: JsonDecoder (Doc GraphicsParam) Unit { boolean :: Boolean, noExists :: Optional (Maybe Int) })
+  runDecoderADE "Roundtrip check"
+    ( (encodeJson :: { noField :: Optional (Maybe Int) } -> Json)
+        { noField: Optional Nothing }
+    )
+    (decodeJson :: JsonDecoder (Doc GraphicsParam) Unit { noField :: Optional (Maybe Int) })
+  log "==="
+
+runDecoderADE
+  :: forall a
+   . Show a
+  => String
+  -> Json
+  -> JsonDecoder (Doc GraphicsParam) Unit a
+  -> Effect Unit
+runDecoderADE msg example decoder =
+  log
+    $ append ("\n" <> msg <> ":\n")
+    $ either printAnsiDodoError show
+    $ runJsonDecoderADE example decoder
+
+exampleRec :: Json
+exampleRec =
+  encodeRecord encoder value
+  where
+  encoder =
+    { boolean: encodeBoolean
+    , number: encodeNumber
+    , string: encodeString
+    , int: encodeInt
+    , object: encodeObject encodeString
+    , array: encodeArray encodeInt
+    , record: encodeRecord
+        { foo: encodeString
+        , bar: encodeUnitToNull
+        }
+    }
+  value =
+    { boolean: true
+    , number: 1.4
+    , string: "hello"
+    , int: 9
+    , object: Object.fromFoldable
+        [ Tuple "a" "1"
+        , Tuple "b" "2"
+        , Tuple "c" "3"
+        , Tuple "d" "4"
+        ]
+    , array: Array.range 1 10
+    , record:
+        { foo: "what"
+        , bar: unit
+        }
+    }
+
+type IncorrectRecordType =
+  { boolean :: String
+  , number :: Boolean
+  , string :: Number
+  , int :: String
+  , object :: Int
+  , array :: Object String
+  , record :: Array Int
+  }

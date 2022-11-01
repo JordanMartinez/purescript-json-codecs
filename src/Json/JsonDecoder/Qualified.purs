60971ee58496c5c0bd6e31e81b53fc7d60b3ca40
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-10-31 22:14:46 -0500

    Fix qualified do compiler error

--- src/Json/JsonDecoder/Qualified.purs
+++ src/Json/JsonDecoder/Qualified.purs
@@ -2,12 +2,13 @@ module Json.JsonDecoder.Qualified where
 
 import Prelude
 
-import Data.Function.Uncurried (mkFn4, runFn4)
+import Codec.Decoder (DecoderFn(..))
+import Data.Function.Uncurried (mkFn5, runFn5)
 import Data.V.Semigroup.Qualified as V
-import Json.JsonDecoder (JsonDecoder(..))
+import Json.JsonDecoder (JsonDecoder)
 
 bind :: forall err extra a b. JsonDecoder err extra a -> (a -> JsonDecoder err extra b) -> JsonDecoder err extra b
-bind (JsonDecoder ma) f = JsonDecoder $ mkFn4 \json pathSoFar handlers extra -> V.do
-  a <- runFn4 ma json pathSoFar handlers extra
-  let (JsonDecoder mb) = f a
-  runFn4 mb json pathSoFar handlers extra
+bind (DecoderFn ma) f = DecoderFn $ mkFn5 \json pathSoFar appendFn handlers extra -> V.do
+  a <- runFn5 ma json pathSoFar appendFn handlers extra
+  let (DecoderFn mb) = f a
+  runFn5 mb json pathSoFar appendFn handlers extra

c489a2b11c1130794238020c50bfdd498709be85
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-12-08 01:23:02 -0600

    Move e/etra ty vars to class head; record issue

--- src/Codec/Json/Bidirectional/Class.purs
+++ src/Codec/Json/Bidirectional/Class.purs
@@ -2,8 +2,8 @@ module Codec.Json.Bidirectional.Class where
 
 import Prelude
 
-import Codec.Json.Bidirectional.Value (array, boolean, codePoint, either, int, json, list, mapCodec, maybe, nonEmpty, nonEmptyArray, nonEmptyList, nonEmptySet, nonEmptyString, nullable, number, object, set, string, these, tuple, unitCodec, voidCodec)
-import Codec.Json.JsonCodec (JsonCodec)
+import Codec.Json.Bidirectional.Value (array, boolean, codePoint, either, int, json, list, mapCodec, maybe, nonEmpty, nonEmptyArray, nonEmptyList, nonEmptySet, nonEmptyString, nullable, number, object, recordPrim, requiredProp, set, string, these, tuple, unitCodec, voidCodec)
+import Codec.Json.JsonCodec (JPropCodec, JsonCodec)
 import Data.Argonaut.Core (Json)
 import Data.Array.NonEmpty (NonEmptyArray)
 import Data.Either (Either)
@@ -17,84 +17,123 @@ import Data.Set (Set)
 import Data.Set.NonEmpty (NonEmptySet)
 import Data.String (CodePoint)
 import Data.String.NonEmpty.Internal (NonEmptyString)
+import Data.Symbol (class IsSymbol)
 import Data.These (These)
 import Data.Tuple (Tuple)
 import Foreign.Object (Object)
+import Prim.Row as Row
+import Prim.RowList as RL
+import Type.Proxy (Proxy(..))
 
-class CodecJson a where
-  codecJson :: forall e extra. JsonCodec e extra a
+class CodecJon e extra a where
+  codecJson :: JsonCodec e extra a
 
-instance CodecJson Json where
+instance CodecJon e extra Json where
   codecJson = json
 
-instance CodecJson Void where
+instance CodecJon e extra Void where
   codecJson = voidCodec
 
-instance CodecJson Unit where
+instance CodecJon e extra Unit where
   codecJson = unitCodec
 
-instance CodecJson Boolean where
+instance CodecJon e extra Boolean where
   codecJson = boolean
 
-instance CodecJson Number where
+instance CodecJon e extra Number where
   codecJson = number
 
-instance CodecJson String where
+instance CodecJon e extra String where
   codecJson = string
 
-instance CodecJson Int where
+instance CodecJon e extra Int where
   codecJson = int
 
-instance CodecJson NonEmptyString where
+instance CodecJon e extra NonEmptyString where
   codecJson = nonEmptyString
 
-instance CodecJson CodePoint where
+instance CodecJon e extra CodePoint where
   codecJson = codePoint
 
-instance CodecJson a => CodecJson (Array a) where
+instance CodecJon e extra a => CodecJon e extra (Array a) where
   codecJson = array codecJson
 
-instance CodecJson a => CodecJson (NonEmptyArray a) where
+instance CodecJon e extra a => CodecJon e extra (NonEmptyArray a) where
   codecJson = nonEmptyArray codecJson
 
--- Record
-
-instance CodecJson a => CodecJson (Object a) where
+instance CodecJon e extra a => CodecJon e extra (Object a) where
   codecJson = object codecJson
 
--- Variant
-
-instance CodecJson a => CodecJson (Nullable a) where
+instance CodecJon e extra a => CodecJon e extra (Nullable a) where
   codecJson = nullable codecJson
 
-instance CodecJson a => CodecJson (Maybe a) where
+instance CodecJon e extra a => CodecJon e extra (Maybe a) where
   codecJson = maybe codecJson
 
-instance (CodecJson a, CodecJson b) => CodecJson (Either a b) where
+instance (CodecJon e extra a, CodecJon e extra b) => CodecJon e extra (Either a b) where
   codecJson = either codecJson codecJson
 
-instance (CodecJson a, CodecJson b) => CodecJson (Tuple a b) where
+instance (CodecJon e extra a, CodecJon e extra b) => CodecJon e extra (Tuple a b) where
   codecJson = tuple codecJson codecJson
 
-instance (CodecJson a, CodecJson b) => CodecJson (These a b) where
+instance (CodecJon e extra a, CodecJon e extra b) => CodecJon e extra (These a b) where
   codecJson = these codecJson codecJson
 
-instance (CodecJson a, CodecJson (f a)) => CodecJson (NonEmpty f a) where
+instance (CodecJon e extra a, CodecJon e extra (f a)) => CodecJon e extra (NonEmpty f a) where
   codecJson = nonEmpty codecJson codecJson
 
-instance (CodecJson a) => CodecJson (List a) where
+instance (CodecJon e extra a) => CodecJon e extra (List a) where
   codecJson = list codecJson
 
-instance (CodecJson a) => CodecJson (NonEmptyList a) where
+instance (CodecJon e extra a) => CodecJon e extra (NonEmptyList a) where
   codecJson = nonEmptyList codecJson
 
-instance (Ord k, CodecJson k, CodecJson v) => CodecJson (Map k v) where
+instance (Ord k, CodecJon e extra k, CodecJon e extra v) => CodecJon e extra (Map k v) where
   codecJson = mapCodec codecJson codecJson
 
-instance (Ord a, CodecJson a) => CodecJson (Set a) where
+instance (Ord a, CodecJon e extra a) => CodecJon e extra (Set a) where
   codecJson = set codecJson
 
-instance (Ord a, CodecJson a) => CodecJson (NonEmptySet a) where
+instance (Ord a, CodecJon e extra a) => CodecJon e extra (NonEmptySet a) where
   codecJson = nonEmptySet codecJson
+
+instance
+  ( RL.RowToList row rl
+  , CodecJsonRecord e extra rl row
+  ) =>
+  CodecJon e extra { | row } where
+  codecJson = recordPrim (unCJPropFn (codecJsonRecord :: CJPropFn e extra rl () row))
+
+newtype CJPropFn :: Type -> Type -> RL.RowList Type -> Row Type -> Row Type -> Type
+newtype CJPropFn e extra rl from to =
+  CJPropFn (JPropCodec e extra { | from } -> JPropCodec e extra { | to })
+
+unCJPropFn
+  :: forall e extra rl from to
+   . CJPropFn e extra rl from to
+  -> (JPropCodec e extra { | from } -> JPropCodec e extra { | to })
+unCJPropFn (CJPropFn fn) = fn
+
+class CodecJsonRecord e extra rl row | e extra rl -> row where
+  codecJsonRecord :: CJPropFn e extra rl () row
+
+instance CodecJsonRecord e extra RL.Nil () where
+  codecJsonRecord = CJPropFn identity
+
+-- HUH!? Compiler: "Unknown type class 'CodecJson'"
+--
+-- instance
+--   ( Row.Cons sym a row' row
+--   , CodecJson e extra a
+--   , CodecJsonRecord e extra tail row'
+--   , IsSymbol sym
+--   ) =>
+--   CodecJsonRecord e extra (RL.Cons sym a tail) row where
+--   codecJsonRecord = CJPropFn
+--     ( requiredProp (Proxy :: Proxy sym) codecJson
+--         <<< (unCJPropFn (codecJsonRecord :: CJPropFn e extra tail () row'))
+--     )
+
+-- Variant
 
 -- Generic

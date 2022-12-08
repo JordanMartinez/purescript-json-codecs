acf1be16caa7cd0be7220635b95de83d9732f020
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-12-08 02:22:46 -0600

    Drop commented-out code in JsonCodec

--- src/Codec/Json/JsonCodec.purs
+++ src/Codec/Json/JsonCodec.purs
@@ -26,61 +26,6 @@ type JsonCodec' e extra from to = Codec' (Array JsonOffset) (JsonErrorHandlers e
 type JIndexedCodec e extra a = Codec (Array JsonOffset) (JsonErrorHandlers e) e extra (Array Json) (List Json) a a
 type JPropCodec e extra a = Codec (Array JsonOffset) (JsonErrorHandlers e) e extra (Object Json) (List (Tuple String Json)) a a
 
--- addOffset :: forall e extra a. JsonOffset -> Json -> JsonCodec e extra a -> JsonCodec e extra a
--- addOffset offset json (DecoderFn f) = DecoderFn $
---   mkFn5 \path appendFn handlers@(JsonErrorHandlers h) extra _ ->
---     runFn5 f (if h.includeJsonOffset then Array.snoc path offset else path) appendFn handlers extra json
-
--- onError :: forall e extra a. (Array JsonOffset -> e -> e) -> JsonCodec e extra a -> JsonCodec e extra a
--- onError mapErrs (DecoderFn f) = DecoderFn $ mkFn5 \path appendFn handlers extra json ->
---   lmap (mapErrs path) $ runFn5 f path appendFn handlers extra json
-
--- failWithMissingField :: forall e extra a. String -> JsonCodec e extra a
--- failWithMissingField str = DecoderFn $ mkFn5 \path _ (JsonErrorHandlers h) _ _ ->
---   invalid $ h.onMissingField path str
-
--- failWithMissingIndex :: forall e extra a. Int -> JsonCodec e extra a
--- failWithMissingIndex idx = DecoderFn $ mkFn5 \path _ (JsonErrorHandlers h) _ _ ->
---   invalid $ h.onMissingIndex path idx
-
--- failWithUnrefinableValue :: forall e extra a. String -> JsonCodec e extra a
--- failWithUnrefinableValue msg = DecoderFn $ mkFn5 \path _ (JsonErrorHandlers h) _ _ ->
---   invalid $ h.onUnrefinableValue path msg
-
--- failWithStructureError :: forall e extra a. String -> JsonCodec e extra a
--- failWithStructureError msg = DecoderFn $ mkFn5 \path _ (JsonErrorHandlers h) _ _ ->
---   invalid $ h.onStructureError path msg
-
--- addTypeHint :: forall e extra a. String -> JsonCodec e extra a -> JsonCodec e extra a
--- addTypeHint s = liftD (JsonDecoder.addTypeHint s)
-
--- addCtorHint :: forall e extra a. String -> JsonCodec e extra a -> JsonCodec e extra a
--- addCtorHint s = liftD (JsonDecoder.addCtorHint s)
-
--- addSubtermHint :: forall e extra a. Int -> JsonCodec e extra a -> JsonCodec e extra a
--- addSubtermHint i = liftD (JsonDecoder.addSubtermHint i)
-
--- addFieldHint :: forall e extra a. String -> JsonCodec e extra a -> JsonCodec e extra a
--- addFieldHint s = liftD (JsonDecoder.addFieldHint s)
-
--- -- | Works like `alt`/`<|>`. Decodes using the first decoder and, if that fails,
--- -- | decodes using the second decoder. Errors from both decoders accumulate.
--- altAccumulate :: forall e extra a. JsonCodec e extra a -> JsonCodec e extra a -> JsonCodec e extra a
--- altAccumulate (DecoderFn f1) (DecoderFn f2) = DecoderFn $ mkFn5 \path appendFn handlers extra json ->
---   case unwrap $ runFn5 f1 path appendFn handlers extra json of
---     Left e -> case unwrap $ runFn5 f2 path appendFn handlers extra json of
---       Left e2 -> invalid $ appendFn e e2
---       Right a -> V $ Right a
---     Right a -> V $ Right a
-
--- -- | Same as `altAccumulate` except only the last error is kept. Helpful in cases
--- -- | where one is decoding a sum type with a large number of data constructors.
--- altLast :: forall e extra a. JsonCodec e extra a -> JsonCodec e extra a -> JsonCodec e extra a
--- altLast (DecoderFn f1) (DecoderFn f2) = DecoderFn $ mkFn5 \path appendFn handlers extra json ->
---   case unwrap $ runFn5 f1 path appendFn handlers extra json of
---     Left _ -> runFn5 f2 path appendFn handlers extra json
---     Right a -> V $ Right a
-
 mkJsonCodec
   ∷ ∀ e extra a
   . JsonDecoder e extra a

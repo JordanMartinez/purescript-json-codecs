e600c6d833a82c4d6c22007ccd61826043636f91
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-12-19 22:15:11 -0600

    Add benchmark results for encoding array of ints

--- bench/Main.purs
+++ bench/Main.purs
@@ -4,10 +4,19 @@ import Prelude
 
 import Benchotron.Core (Benchmark, benchFn, mkBenchmark, unpackBenchmark)
 import Benchotron.UI.Console (benchmarkToFile)
+import Codec.Codec as Codec
+import Codec.Json.Bidirectional.Class as BiC
+import Codec.Json.Bidirectional.Value as BiV
+import Codec.Json.Unidirectional.Encode.Class as UniCE
+import Codec.Json.Unidirectional.Encode.Value as UniV
+import Data.Argonaut.Encode as AE
 import Data.Array as Array
-import Data.List (List(..), (:))
+import Data.Codec as CC
+import Data.Codec.Argonaut as CA
 import Data.Foldable (foldr, traverse_)
 import Data.Formatter.DateTime (FormatterCommand(..), format)
+import Data.Function.Uncurried (runFn2)
+import Data.List (List(..), (:))
 import Effect (Effect)
 import Effect.Now (nowDateTime)
 import Node.Path as Path
@@ -16,7 +25,7 @@ import Test.QuickCheck.Gen (Gen, vectorOf)
 
 main :: Effect Unit
 main = traverse_ benchIt
-  [ benchSum ]
+  [ benchEncodeArrayInt ]
 
 benchIt :: Benchmark -> Effect Unit
 benchIt b = do
@@ -34,15 +43,27 @@ benchIt b = do
   yyyymmdd = inBetween (Placeholder "-") $ YearFull : MonthTwoDigits : DayOfMonthTwoDigits : Nil
   hhmmss = inBetween (Placeholder ":") $ Hours24 : MinutesTwoDigits : SecondsTwoDigits : Nil
 
-benchSum :: Benchmark
-benchSum = mkBenchmark
-  { slug: "array-of-ints"
-  , title: "Finding the sum of an array"
+benchEncodeArrayInt :: Benchmark
+benchEncodeArrayInt = mkBenchmark
+  { slug: "encode--array-of-ints"
+  , title: "Encoding an array of ints"
   , sizes: 1 Array... 5 <#> (_ * 1_000)
   , sizeInterpretation: "Number of elements in the array"
   , inputsPerSize: 1
   , gen: \n -> vectorOf n (arbitrary :: Gen Int)
   , functions:
-      [ benchFn "foldr" Array.length
+      [ benchFn "encode (argonaut-codec)"
+          AE.encodeJson
+      , benchFn "encode (json-codecs - uni - value)" $
+          UniV.encodeArray UniV.encodeInt
+      , benchFn "encode (json-codecs - uni - class)"
+          UniCE.encodeJson
+      , benchFn "encode (codec-argonaut)"
+          $ CC.encode
+          $ CA.array CA.int
+      , benchFn "encode (json-codecs - bidi - value)" $
+          \a -> runFn2 (Codec.encoder $ BiV.array BiV.int) unit a
+      , benchFn "encode (json-codecs - bidi - class)" $
+          \a -> runFn2 (Codec.encoder BiC.codecJson) unit a
       ]
   }

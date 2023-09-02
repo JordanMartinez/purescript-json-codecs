5393d675936d530facff9e78667b4fc582854487
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-09-02 17:31:11 -0500

    Refactor how snapshots are called

--- snapshots/Test/Snapshot.purs
+++ snapshots/Test/Snapshot.purs
@@ -3,27 +3,25 @@ module Test.Snapshot where
 import Prelude
 
 import Data.Foldable (for_)
-import Data.Maybe (Maybe(..))
 import Data.String (Pattern(..))
 import Data.String as String
 import Effect (Effect)
 import Effect.Aff (launchAff_)
-import Node.ChildProcess (defaultExecSyncOptions, execSync)
+import Effect.Class (liftEffect)
 import Node.FS.Aff as FSA
+import Node.FS.Sync as FS
 import Node.Path as Path
 
 main :: Effect Unit
 main = do
-  void $ execSync "spago -x snapshots.dhall build --purs-args \"-g corefn\"" defaultExecSyncOptions
-  void $ execSync "purs-backend-es build" $ defaultExecSyncOptions { cwd = Just "snapshots" }
-  let
-    snapshotsDir = "snapshots"
-    outputEsDir = Path.concat [ snapshotsDir, "output-es" ]
+  let outputEsDir = "output-es"
   launchAff_ do
     mods <- FSA.readdir outputEsDir
     for_ mods \mod ->
       for_ (String.stripPrefix (Pattern "Snapshot.") mod) \modName -> do
         let
           jsFile = Path.concat [ outputEsDir, mod, "index.js" ]
-          snapshotFile = Path.concat [ snapshotsDir, "Snapshot", modName <> "-snapshot.js" ]
+          snapshotFile = Path.concat [ "snapshots", "Snapshot", modName <> "-snapshot.js" ]
+        whenM (liftEffect $ FS.exists snapshotFile) do
+          FSA.unlink snapshotFile
         FSA.copyFile jsFile snapshotFile

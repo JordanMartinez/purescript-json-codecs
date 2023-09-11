31060a0b503e638b4a8aea1a509c2495444941ea
Jordan Martinez <jordanalex.martinez@protonmail.com>
2023-09-11 09:13:11 -0500

    Add purs compile output for snapshots

--- snapshots/Test/Snapshot.purs
+++ snapshots/Test/Snapshot.purs
@@ -6,7 +6,7 @@ import Data.Foldable (for_)
 import Data.String (Pattern(..))
 import Data.String as String
 import Effect (Effect)
-import Effect.Aff (launchAff_)
+import Effect.Aff (Aff, launchAff_)
 import Effect.Class (liftEffect)
 import Node.FS.Aff as FSA
 import Node.FS.Sync as FS
@@ -14,14 +14,22 @@ import Node.Path as Path
 
 main :: Effect Unit
 main = do
-  let outputEsDir = "output-es"
   launchAff_ do
-    mods <- FSA.readdir outputEsDir
-    for_ mods \mod ->
-      for_ (String.stripPrefix (Pattern "Snapshot.") mod) \modName -> do
-        let
-          jsFile = Path.concat [ outputEsDir, mod, "index.js" ]
-          snapshotFile = Path.concat [ "snapshots", "Snapshot", modName <> "-snapshot.js" ]
-        whenM (liftEffect $ FS.exists snapshotFile) do
-          FSA.unlink snapshotFile
-        FSA.copyFile jsFile snapshotFile
+    copyFile "output-es"
+      (\outputEsDir mod _ -> Path.concat [ outputEsDir, mod, "index.js" ])
+      (\_ _ modName -> Path.concat [ "snapshots", "Snapshot", modName <> "-snapshot.js" ])
+    copyFile "output"
+      (\outputDir mod _ -> Path.concat [ outputDir, mod, "index.js" ])
+      (\_ _ modName -> Path.concat [ "snapshots", "Snapshot", modName <> "-index.js" ])
+
+copyFile :: String -> (String -> String -> String -> String) -> (String -> String -> String -> String) -> Aff Unit
+copyFile outputDir toJsFile toOutFile = do
+  mods <- FSA.readdir outputDir
+  for_ mods \mod ->
+    for_ (String.stripPrefix (Pattern "Snapshot.") mod) \modName -> do
+      let
+        jsFile = toJsFile outputDir mod modName
+        snapshotFile = toOutFile outputDir mod modName
+      whenM (liftEffect $ FS.exists snapshotFile) do
+        FSA.unlink snapshotFile
+      FSA.copyFile jsFile snapshotFile
